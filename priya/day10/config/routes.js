4c11d3650170c4899ac07720818e01a97f6534ef
unknown <aniket@aniket-PC>
2018-10-31 01:06:29 +0530

    day 10 routing done by priya

---
+++ priya/day10/config/routes.js
@@ -0,0 +1,36 @@
+var express=require("express");
+var routes= express.Router();
+
+routes.use("/",require("../controller/home"));
+routes.use("/about",require("../controller/about"));
+routes.use("/contact",require ("../controller/contact"));
+//routes.use("/login",require("../controller/login"));
+module.exports=routes;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
