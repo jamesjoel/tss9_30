178e290f93c4d0bf26336616adb0f76b55d84955
jamesjoel <james.joelj@gmail.com>
2018-12-13 11:16:31 +0530

    socket

---
+++ socket/app.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+
+app.get("/", function(req, res){
+
+	res.sendFile(__dirname+"/index.html");
+});
+ 
+io.on("connection", function(socket){
+	// console.log("page call-----", socket.id);
+	socket.on("sending", function(data){
+
+		socket.broadcast.emit("send_to_server", data);
+	});
+
+
+
+
+
+	socket.on("disconnect", function(){
+		console.log("a user dissconnect");
+	}); 
+});
+
+
+
+
+
+http.listen(3000, function(){
+	console.log("Server Running");
+});
