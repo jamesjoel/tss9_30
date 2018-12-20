0cf481b20b19ceb0d90c3c299b2b86d39f6cd6f8
pri0310 <priyanka.jain66@gmail.com>
2018-12-20 19:19:27 +0530

    angular with AJAX- Priyanka

--- priyanka/angular/app.js
+++ priyanka/angular/app.js
@@ -1,5 +1,6 @@
 var express=require("express");
 var app = express();
+var MongoClient = require("mongodb").MongoClient;
 
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
@@ -8,9 +9,19 @@ app.get("/", function(req, res){
 	res.render("index");
 });
 
+app.get("/demo",function(req,res){
+	MongoClient.connect("mongodb://localhost:27017",function(err,client){
+		var db=client.db("tss9");
+		db.collection("product").find().toArray(function(err,result){
+			res.send(result);
+		});
+	});
+});
+
 app.listen(3000, function(err){
 	if(err){
 		console.log("Server error",err);
 		return;	}
 		console.log("server running");
 });
+
