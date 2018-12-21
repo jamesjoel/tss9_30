82607ddd0fe7b0e4db7631e5bc619e0132aa0822
pri0310 <priyanka.jain66@gmail.com>
2018-12-21 22:57:06 +0530

    Angular and AJAX-Priyanka

--- priyanka/angular/public/js/app.js
+++ priyanka/angular/public/js/app.js
@@ -1,15 +1,17 @@
 var app = angular.module("myApp",[]);
 app.controller("myCtrl",function($scope,$http){
-	$scope.name = "rohit";
-	$scope.age = 25;
-	$scope.demo=function(){
+	$scope.newData = {};
+	$scope.save=function(){
 		$http({
-			url:"/demo",
-			method:"get"
+			url:"/",
+			data:$scope.newData,
+			method:"post"
 		}).then(function(res){
-			console.log(res.data);
-			$scope.allData=res.data;
+			if(res.data){
+			$("#msgModal").modal("show");	
+			}else{
+				$("#newModal").modal("show");
+			}
 		});
 	}
 });	
-
