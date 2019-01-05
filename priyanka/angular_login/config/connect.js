23906048d39d5c24e8dc1830dd2b04e1da27271d
pri0310 <priyanka.jain66@gmail.com>
2019-01-05 19:36:35 +0530

    Angular login complete-priyanka

--- priyanka/angular_login/config/connect.js
+++ priyanka/angular_login/config/connect.js
@@ -1,5 +1,5 @@
 var MongoClient=require("mongodb").MongoClient;
-var url="mongodb+srv://priyanka:pj30889#@cluster0-piea5.mongodb.net/test?retryWrites=true";
+var url="mongodb://priyanka:pj03@cluster0-shard-00-00-piea5.mongodb.net:27017,cluster0-shard-00-01-piea5.mongodb.net:27017,cluster0-shard-00-02-piea5.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
 var client=new MongoClient(url,{useNewUrlParser:true});
 module.exports=function(cb){
 	client.connect(cb);
--- priyanka/angular_login/public/js/app.js
+++ priyanka/angular_login/public/js/app.js
@@ -1,5 +1,5 @@
 var app=angular.module("myApp",[]);
-app.controller("loginCtrl",function($scope,$http){
+app.controller("loginCtrl",function($scope,$http,$window){
 	$scope.loginData={};
 	$scope.preloaderShow=false;
 	$scope.showBox=false;
@@ -8,20 +8,36 @@ app.controller("loginCtrl",function($scope,$http){
 		if(!$scope.loginData.username){
 			$scope.msg="Enter Your Email/Username";
 		}else{
-			$scope.msg="msg";
-			// $scope.preloaderShow=true;
+			$scope.msg="";
+			$scope.preloaderShow=true;
 			$http({
 				url:"/webservices/checkusername",
 				data:$scope.loginData,
 				method:"post"
 			}).then(function(res){
-				// $scope.preloaderShow=false;
-				if(res.data==0){
-					$scope.msg="Inavalid Username/Email";
+				$scope.preloaderShow=false;
+				if(res.data[0]==0){
+					$scope.msg="Invalid Username/Email";
 				}else{
-					// $scope.showBox=true;
+					$scope.showBox=true;
+					$scope.userData=res.data[1];
 				}
 			});
 		}
+	}
+	$scope.do_login=function(){
+		$scope.preloaderShow=true;
+		$http({
+			url:"/webservices/checkpassword",
+			data:$scope.loginData,
+			method:"post"
+		}).then(function(res){
+			if(res.data==0){
+				$scope.preloaderShow=false;
+				$scope.msg="Inavalid Password";
+			}else{
+				$window.location.href="/user";
+			}
+		});
 	}
 });
